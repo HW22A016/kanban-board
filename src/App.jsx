5fff10617d33cd5139b43aa1cb882b3ac09bb361
HW22A016 <hw22a016@oecu.jp>
2026-09-10 15:22:56 +0900

    タスク追加の色の指定を廃止

--- src/App.jsx
+++ src/App.jsx
@@ -308,8 +308,8 @@ function App() {
       </div>
 
       <div style={{marginBottom: "10px"}}>
-        <p style={{color: "#000"}}>タスクの追加</p>
-        <div style={{color: "#000", display:"flex", justifyContent:"center"}}>
+        <p>タスクの追加</p>
+        <div style={{display:"flex", justifyContent:"center"}}>
           <div style={{marginBottom: "10px", textAlign: "right"}}>
             <div>
               <input
